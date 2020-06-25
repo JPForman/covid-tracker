8de73ae8aa57ee8f6ebfe16a2b434dc00e168212
Joshua Forman <cyborgforman@gmail.com>
2020-06-24 19:08:20 -0700

    add simple api call

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api';
 
 import styles from './App.module.css';
 
 export default class App extends Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log('data: ', data);
+    
+  }
+
   render() {
     return (
       <div className={styles.container}>
