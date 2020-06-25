6c7226c3fb68257e84d960d7c77c8f78d13560c7
Joshua Forman <cyborgforman@gmail.com>
2020-06-24 18:58:06 -0700

    create initial components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     hello world
-    </div>
-  );
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import styles from './App.module.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Chart() {
+  return (
+    <div>
+      <h1>Charts dude</h1>
+    </div>
+  )
+}
